39fb327625935e4d95ecb6dbfd7d7d370855937e
Erich P. Lotto <erichlotto@users.noreply.github.com>
2019-09-05 18:10:48 -0300

    Update START.sh

--- START.sh
+++ START.sh
@@ -39,8 +39,10 @@ sudo apt update
 sudo apt install -y enpass
 
 echo "Installing Ledger Live..."
-curl https://lh3.googleusercontent.com/vxu1hbx9CIeneYOSLrExa3mbpKHwok-Tg831G14hh7jSA38Xcmpv19HrIloQi-XjwViB --create-dirs -o ~/.local/share/icons/appimagekit-ledger-live-desktop.png
-curl https://download-live.ledger.com/releases/latest/download/linux --create-dirs -o ~/.local/share/applications/ledger-live.AppImage
+mkdir -p ~/.local/share/icons
+wget -O ~/.local/share/icons/appimagekit-ledger-live-desktop.png https://lh3.googleusercontent.com/vxu1hbx9CIeneYOSLrExa3mbpKHwok-Tg831G14hh7jSA38Xcmpv19HrIloQi-XjwViB
+mkdir -p ~/.local/share/applications
+wget -O ~/.local/share/applications/ledger-live.AppImage https://download-live.ledger.com/releases/latest/download/linux
 sudo chmod +x ~/.local/share/applications/ledger-live.AppImage
 ~/.local/share/applications/ledger-live.AppImage
 
