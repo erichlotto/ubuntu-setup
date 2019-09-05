186b6ca14430cad7292669f6679097a9d4bdbe36
Erich P. Lotto <erichlotto@users.noreply.github.com>
2019-09-05 18:57:55 -0300

    Create install-enpass.sh

---
+++ install-enpass.sh
@@ -0,0 +1,4 @@
+sudo sh -c 'echo "deb https://apt.enpass.io/ stable main" > /etc/apt/sources.list.d/enpass.list'
+wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo apt-key add -
+sudo apt update
+sudo apt install -y enpass
