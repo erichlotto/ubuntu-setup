e6bbe39b8524a961dac7027b822a8226930cc9f2
Erich P. Lotto <erichlotto@users.noreply.github.com>
2019-09-05 19:05:45 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,17 @@ rm -rf .ubuntu-setup
 mkdir -p .ubuntu-setup && cd .ubuntu-setup
 echo -n "📄 Downloading scripts... "
 wget -q https://github.com/erichlotto/ubuntu-setup/blob/master/helpers.sh
+wget -q https://github.com/erichlotto/ubuntu-setup/blob/master/create-templates.sh
 wget -q https://github.com/erichlotto/ubuntu-setup/blob/master/install-chrome.sh
 wget -q https://github.com/erichlotto/ubuntu-setup/blob/master/install-enpass.sh
 wget -q https://github.com/erichlotto/ubuntu-setup/blob/master/install-snapcraft.sh
 wget -q https://github.com/erichlotto/ubuntu-setup/blob/master/install-vscode.sh
+echo "DONE"
+
+echo -n "Fixing permissions... "
+sudo chmod +x *
+echo "DONE"
+
+echo "Installing Chrome..."
+./install-chrome.sh
 echo "DONE"
