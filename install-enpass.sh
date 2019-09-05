130e5419795dc3e92ac69420bb583fa89bd33bcf
Erich P. Lotto <erichlotto@users.noreply.github.com>
2019-09-05 19:51:45 -0300

    Update install-enpass.sh

--- install-enpass.sh
+++ install-enpass.sh
@@ -3,4 +3,4 @@
 sudo sh -c 'echo "deb https://apt.enpass.io/ stable main" > /etc/apt/sources.list.d/enpass.list'
 wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo apt-key add -
 sudo apt update
-sudo apt install -y enpass
+sudo apt install -qq -y enpass
