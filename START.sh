37dfa26524695e3ded4d2f3b15016c9ed5da9520
Erich P. Lotto <erichlotto@users.noreply.github.com>
2019-09-05 16:10:40 -0300

    Update START.sh

--- START.sh
+++ START.sh
@@ -37,3 +37,9 @@ sudo echo "deb https://apt.enpass.io/ stable main" > /etc/apt/sources.list.d/enp
 sudo wget -O - https://apt.enpass.io/keys/enpass-linux.key | apt-key add -
 sudo apt update
 sudo apt install -y enpass
+
+echo "Installing Ledger Live..."
+wget -O ~/.local/share/icons/appimagekit-ledger-live-desktop.png https://lh3.googleusercontent.com/vxu1hbx9CIeneYOSLrExa3mbpKHwok-Tg831G14hh7jSA38Xcmpv19HrIloQi-XjwViB
+wget -O ~/.local/share/applications/ledger-live.AppImage https://download-live.ledger.com/releases/latest/download/linux
+sudo chmod +x ~/.local/share/applications/ledger-live.AppImage
+~/.local/share/applications/ledger-live.AppImage
